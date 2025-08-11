636315786fa8dfc3fe2855b3339906e635dd4e2f
SophieMoon16 <sluu160593@gmail.com>
2025-08-11 10:54:41 +0200

    V10

--- src/app/components/todo-detail/todo-detail.component.ts
+++ src/app/components/todo-detail/todo-detail.component.ts
@@ -7,6 +7,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-todo-detail',
+  standalone: true, // <= important
   templateUrl: './todo-detail.component.html',
   styleUrls: ['./todo-detail.component.css'],
 })
